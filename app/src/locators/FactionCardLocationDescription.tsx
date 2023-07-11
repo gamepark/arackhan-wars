79904e0e2a752307e8de274126b48fa752a8b397
Romain Fromi <romain.fromi@gmail.com>
2023-07-11 16:42:40 +0200

    Move "isMoveToLocation" & "isDropLocation" from ItemLocator to LocationDescription

--- app/src/locators/FactionCardLocationDescription.tsx
+++ app/src/locators/FactionCardLocationDescription.tsx
@@ -5,12 +5,13 @@ import { LocationDescription } from '@gamepark/react-game'
 import { PlayerId } from '@gamepark/arackhan-wars/ArackhanWarsOptions'
 import { css } from '@emotion/react'
 import { factionCardDescription } from '../material/FactionCardDescription'
+import { Location, MaterialMove, MoveKind } from '../../../../workshop/packages/rules-api'
+import { CustomMoveType } from '@gamepark/arackhan-wars/material/CustomMoveType'
 
 export class FactionCardLocationDescription extends LocationDescription<PlayerId, MaterialType, LocationType> {
   alwaysVisible = false
   height = factionCardDescription.width / factionCardDescription.ratio
   width = factionCardDescription.width
-
 
   getExtraCss() {
     return css`
@@ -18,5 +19,9 @@ export class FactionCardLocationDescription extends LocationDescription<PlayerId
       width: 100%;
       border-radius: inherit;
     `
+  }
+
+  isDropLocation = (move: MaterialMove, location: Location): boolean => {
+    return move.kind === MoveKind.CustomMove && move.type === CustomMoveType.Attack && (move.data.targets ?? []).includes(location.parent)
   }
 }
--- app/src/locators/FactionCardLocator.tsx
+++ app/src/locators/FactionCardLocator.tsx
@@ -1,19 +1,12 @@
 /** @jsxImportSource @emotion/react */
 import { MaterialType } from '@gamepark/arackhan-wars/material/MaterialType'
 import { LocationType } from '@gamepark/arackhan-wars/material/LocationType'
-import { Location, MaterialMove, MoveKind, XYCoordinates } from '@gamepark/rules-api'
 import { ItemLocator } from '@gamepark/react-game'
 import { PlayerId } from '@gamepark/arackhan-wars/ArackhanWarsOptions'
-import { CustomMoveType } from '@gamepark/arackhan-wars/material/CustomMoveType'
 import { FactionCardLocationDescription } from './FactionCardLocationDescription'
 
 export class FactionCardLocator extends ItemLocator<PlayerId, MaterialType, LocationType> {
   locationDescription = new FactionCardLocationDescription()
   parentItemType = MaterialType.FactionCard
-
-  getPositionOnParent = (): XYCoordinates => ({ x: 50, y: 50 })
-
-  isDropLocation = (move: MaterialMove, location: Location): boolean => {
-    return move.kind === MoveKind.CustomMove && move.type === CustomMoveType.Attack && (move.data.targets ?? []).includes(location.parent)
-  }
+  positionOnParent = { x: 50, y: 50 }
 }
