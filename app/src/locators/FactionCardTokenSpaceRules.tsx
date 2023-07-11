5b9cb911d38569c737855fb55d44c32a406ff17d
Romain Fromi <romain.fromi@gmail.com>
2023-07-11 12:04:08 +0200

    New API for Location Rules

--- app/src/locators/FactionCardTokenSpaceRules.tsx
+++ app/src/locators/FactionCardTokenSpaceRules.tsx
@@ -1,10 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { LocationRulesProps } from '@gamepark/react-game'
-import { MaterialType } from '@gamepark/arackhan-wars/material/MaterialType'
-import { LocationType } from '@gamepark/arackhan-wars/material/LocationType'
-import { PlayerId } from '@gamepark/arackhan-wars/ArackhanWarsOptions'
 
-export const FactionCardTokenSpaceRules = (_props: LocationRulesProps<PlayerId, MaterialType, LocationType>) => {
+export const FactionCardTokenSpaceRules = (_props: LocationRulesProps) => {
   return <>
 
   </>
