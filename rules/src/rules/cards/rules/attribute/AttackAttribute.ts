6bd3fb542390b0f50cc372d8774899391637f38e
Romain Fromi <romain.fromi@gmail.com>
2023-07-21 16:50:13 +0200

    Fix typo

--- rules/src/rules/cards/rules/attribute/AttackAttribute.ts
+++ rules/src/rules/cards/rules/attribute/AttackAttribute.ts
@@ -4,7 +4,7 @@ import { MaterialMove } from '@gamepark/rules-api/dist/material/moves/MaterialMo
 
 
 export abstract class AttackAttributeRule extends AttributeRule {
-  abstract OgetLegalAttacks(_attacker: Material, opponentsCards: Material): MaterialMove[]
+  abstract getLegalAttacks(_attacker: Material, opponentsCards: Material): MaterialMove[]
 
   getAttackValue(attack: number, _attacker: Material, _opponent: Material): number {
     return attack
