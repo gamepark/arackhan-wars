0f9aa1e2bf1db9c7b7022d3846859036ac46bc31
Romain Fromi <romain.fromi@gmail.com>
2023-07-28 11:33:07 +0200

    Export ArackhanWarsSetup

--- rules/src/index.ts
+++ rules/src/index.ts
@@ -1,3 +1,4 @@
 export { ArackhanWarsRules } from './ArackhanWarsRules'
 export { GameOptionsSpec } from './ArackhanWarsOptions'
+export { ArackhanWarsSetup } from './ArackhanWarsSetup'
 export { battlefieldSpaceCoordinates } from './material/spaces'
