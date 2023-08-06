58ab1d1b7208f45c01738233a80653cac0594f00
Romain Fromi <romain.fromi@gmail.com>
2023-08-06 19:05:40 +0200

    Bugfix: memorize cannot be done un getAutomaticMove

--- rules/src/rules/RevealRule.ts
+++ rules/src/rules/RevealRule.ts
@@ -1,6 +1,6 @@
 import { MaterialType } from '../material/MaterialType'
 import { LocationType } from '../material/LocationType'
-import { ItemMove, ItemMoveType, MaterialMove, MaterialRulesPart, MoveKind } from '@gamepark/rules-api'
+import { ItemMove, ItemMoveType, MaterialRulesPart, MoveKind } from '@gamepark/rules-api'
 import { RuleId } from './RuleId'
 import { PlayerId } from '../ArackhanWarsOptions'
 import { onBattlefieldAndAstralPlane } from '../utils/LocationUtils'
@@ -10,20 +10,19 @@ import { getCardRule } from './cards/rules/base/CardRule'
 
 export class RevealRule extends MaterialRulesPart<PlayerId, MaterialType, LocationType> {
 
-  getAutomaticMoves(): MaterialMove<PlayerId, MaterialType, LocationType>[] {
+  getAutomaticMoves() {
     const revealCards = this.material(MaterialType.FactionCard)
       .location(onBattlefieldAndAstralPlane)
       .filter(item => !!item.rotation?.y)
       .moveItems({ rotation: {} })
 
-    this.memorize(Memory.IsInitiativeSequence, true)
     return [
       ...revealCards,
       this.rules().startPlayerTurn(RuleId.ActivationRule, this.remind(Memory.StartPlayer))
     ]
   }
 
-  afterItemMove(move: ItemMove<PlayerId, MaterialType, LocationType>): MaterialMove<PlayerId, MaterialType, LocationType>[] {
+  afterItemMove(move: ItemMove<PlayerId, MaterialType, LocationType>) {
     if (move.kind === MoveKind.ItemMove && move.type === ItemMoveType.Move) {
       const revealedCard = this.material(move.itemType).getItem(move.itemIndex)!
       if (isSpell(getCardRule(this.game, move.itemIndex).characteristics)) return []
@@ -38,6 +37,11 @@ export class RevealRule extends MaterialRulesPart<PlayerId, MaterialType, Locati
       ]
     }
 
+    return []
+  }
+
+  onRuleEnd() {
+    this.memorize(Memory.IsInitiativeSequence, true)
     return []
   }
 }
