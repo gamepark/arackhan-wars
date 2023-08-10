2f563141f399171f712735d3c262a81ac14ae72a
Patrick Beaumé <p.beaume@gmail.com>
2023-08-10 22:18:35 +0200

    add faction token help

--- app/src/material/FactionTokenRules.tsx
+++ app/src/material/FactionTokenRules.tsx
@@ -1,7 +1,14 @@
 /** @jsxImportSource @emotion/react */
+import { Trans, useTranslation } from 'react-i18next';
+import { MaterialRulesProps } from '@gamepark/react-game/dist/components/material/MaterialDescription';
 
-export const FactionTokenRules = () => {
+export const FactionTokenRules = (props: MaterialRulesProps) => {
+  const { item } = props;
+  const { t } = useTranslation()
+  const flipped = item.rotation?.y === 1
   return <>
-
+    <h2>{t('rules.faction-token.title')}</h2>
+    <p>{t('rules.faction-token.purpose')}</p>
+    {flipped && <p><Trans defaults='rules.faction-token.flipped'><strong /></Trans></p> }
   </>
 }
