6d5921028842e2112724846fdb008bfc45e53ce0
Patrick Beaumé <p.beaume@gmail.com>
2023-08-10 23:33:24 +0200

    add round tracker rules

--- app/src/material/RoundTrackerRules.tsx
+++ app/src/material/RoundTrackerRules.tsx
@@ -1,7 +1,17 @@
 /** @jsxImportSource @emotion/react */
+import { useTranslation } from 'react-i18next';
+import { ArackhanWarsRules } from '@gamepark/arackhan-wars/ArackhanWarsRules';
+import { useRules } from '@gamepark/react-game/dist/hooks/useRules';
+import { MaterialType } from '@gamepark/arackhan-wars/material/MaterialType';
 
 export const RoundTrackerRules = () => {
+  const { t } = useTranslation()
+  const rules = useRules<ArackhanWarsRules>()!
+  const tracker = rules.material(MaterialType.RoundTrackerToken).getItem()!
   return <>
-
+    <h2>{t('rules.round-tracker.title')}</h2>
+    <p>{t('rules.round-tracker.purpose')}</p>
+    <hr />
+    <p>{t('rules.actual-round', { round: tracker.location!.x })}</p>
   </>
 }
--- app/src/material/RoundTrackerTokenRules.tsx
+++ app/src/material/RoundTrackerTokenRules.tsx
@@ -1,7 +1,14 @@
 /** @jsxImportSource @emotion/react */
+import { MaterialRulesProps } from '@gamepark/react-game/dist/components/material/MaterialDescription';
+import { useTranslation } from 'react-i18next';
 
-export const RoundTrackerTokenRules = () => {
+export const RoundTrackerTokenRules = (props: MaterialRulesProps) => {
+  const { item } = props;
+  const { t } = useTranslation()
   return <>
-
+    <h2>{t('rules.round-tracker-token.title')}</h2>
+    <p>{t('rules.round-tracker-token.purpose')}</p>
+    <hr />
+    <p>{t('rules.actual-round', { round: item.location!.x })}</p>
   </>
 }
