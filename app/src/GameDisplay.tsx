413427cd0d97f16f5622a32ed6e9057f133af5c4
Romain Fromi <romain.fromi@gmail.com>
2023-08-22 15:45:52 +0200

    Add custom background

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -2,10 +2,11 @@
 import { GameTable } from '@gamepark/react-game'
 import { PlayerPanels } from './panels/PlayerPanels'
 import { pointerWithin } from '@dnd-kit/core'
+import background from './images/background.jpg'
 
 export default function GameDisplay() {
   return <>
-    <GameTable xMin={-29} xMax={73} yMin={-29} yMax={29} collisionAlgorithm={pointerWithin}/>
+    <GameTable xMin={-29} xMax={73} yMin={-29} yMax={29} collisionAlgorithm={pointerWithin} background={background} backgroundOverlay="rgba(0, 0, 0, 0.3)"/>
     <PlayerPanels/>
   </>
 }
