f5483f210511072bbcf1eca22b2ad6cc6d683700
Romain Fromi <romain.fromi@gmail.com>
2023-08-27 15:25:02 +0200

    Rules dialog for faction tokens

--- app/src/material/FactionTokenRules.tsx
+++ app/src/material/FactionTokenRules.tsx
@@ -1,14 +1,19 @@
 /** @jsxImportSource @emotion/react */
-import { Trans, useTranslation } from 'react-i18next';
-import { MaterialRulesProps } from '@gamepark/react-game/dist/components/material/MaterialDescription';
+import { Trans, useTranslation } from 'react-i18next'
+import { MaterialRulesProps } from '@gamepark/react-game/dist/components/material/MaterialDescription'
+import { TokenFaction } from '@gamepark/arackhan-wars/material/TokenFaction'
 
 export const FactionTokenRules = (props: MaterialRulesProps) => {
-  const { item } = props;
+  const { item } = props
   const { t } = useTranslation()
   const flipped = item.rotation?.y === 1
   return <>
-    <h2>{t('rules.faction-token.title', { faction: t(`faction.${item.id}`)})}</h2>
+    <h2>
+      {item.id === TokenFaction.Neutral ? t('rules.faction-token.neutral.title')
+        : t('rules.faction-token.title', { faction: t(`faction.${item.id}`) })}
+    </h2>
     <p>{t('rules.faction-token.purpose')}</p>
-    {flipped && <p><Trans defaults='rules.faction-token.flipped'><strong /></Trans></p> }
+    {item.id === TokenFaction.Neutral && <p>{t('rules.faction-token.neutral.purpose')}</p>}
+    {flipped && <p><Trans defaults="rules.faction-token.flipped"><strong/></Trans></p>}
   </>
 }
