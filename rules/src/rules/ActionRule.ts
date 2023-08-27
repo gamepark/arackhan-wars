92c00f9186fbd39a6dab44dcf83c8e98372ca9e3
Romain Fromi <romain.fromi@gmail.com>
2023-08-27 13:40:25 +0200

    Prevent players for triggering card actions that cannot be completed

--- rules/src/rules/ActionRule.ts
+++ rules/src/rules/ActionRule.ts
@@ -1,4 +1,4 @@
-import { MaterialMove, PlayerTurnRule } from '@gamepark/rules-api'
+import { isDeadEndMove, MaterialMove, PlayerTurnRule } from '@gamepark/rules-api'
 import { MaterialType } from '../material/MaterialType'
 import { CustomMoveType } from '../material/CustomMoveType'
 import { PlayerId } from '../ArackhanWarsOptions'
@@ -7,24 +7,26 @@ import { getCardRule } from './CardRule'
 import { Memory } from './Memory'
 import { Attack } from './AttackRule'
 import { onBattlefieldAndAstralPlane } from '../material/Board'
+import { ArackhanWarsRules } from '../ArackhanWarsRules'
+import { RuleId } from './RuleId'
 
 export class ActionRule extends PlayerTurnRule<PlayerId, MaterialType, LocationType> {
   getPlayerMoves(): MaterialMove[] {
     return this.availableCards
       .filter(index => getCardRule(this.game, index).canPerformAction)
       .map(index => this.rules().customMove(CustomMoveType.PerformAction, index))
+      .filter(move => !isDeadEndMove(move, this.game, ArackhanWarsRules, game => game.rule?.id === RuleId.ActivationRule))
   }
 
   get availableCards() {
+    if (this.remind<Attack[]>(Memory.Attacks).length > 0) return []
     const movedCard = this.remind<number[]>(Memory.MovedCards)
     if (movedCard.length) {
       return [movedCard[0]]
     }
-    const attacks = this.remind<Attack[]>(Memory.Attacks)
     return this.material(MaterialType.FactionCard)
       .location(onBattlefieldAndAstralPlane)
       .player(this.player)
       .getIndexes()
-      .filter(index => !attacks.some(attack => attack.card === index))
   }
 }
