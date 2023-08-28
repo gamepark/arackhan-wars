0b95b645e9343ee217d5ff4705a4ffa4a523e192
Romain Fromi <romain.fromi@gmail.com>
2023-08-28 13:50:14 +0200

    Display player's discard

--- app/src/locators/PlayerDiscardRules.tsx
+++ app/src/locators/PlayerDiscardRules.tsx
@@ -1,8 +1,38 @@
 /** @jsxImportSource @emotion/react */
-import { LocationRulesProps } from '@gamepark/react-game'
+import { LocationRulesProps, MaterialComponent, pointerCursorCss, usePlay, usePlayerName, useRules } from '@gamepark/react-game'
+import { useTranslation } from 'react-i18next'
+import { ArackhanWarsRules } from '@gamepark/arackhan-wars/ArackhanWarsRules'
+import { MaterialType } from '@gamepark/arackhan-wars/material/MaterialType'
+import { LocationType } from '@gamepark/arackhan-wars/material/LocationType'
+import { displayMaterialRules } from '@gamepark/rules-api'
+import { css } from '@emotion/react'
 
-export const PlayerDiscardRules = (_props: LocationRulesProps) => {
+export const PlayerDiscardRules = ({ location }: LocationRulesProps) => {
+  const { t } = useTranslation()
+  const player = usePlayerName(location.player)
+  const cards = useRules<ArackhanWarsRules>()?.material(MaterialType.FactionCard).location(LocationType.PlayerDiscard).player(location.player)
+    .sort(item => -item.location.x!)
+  const play = usePlay()
   return <>
-
+    <h2>{t('rules.discard.title', { player })}</h2>
+    <p>{t('rules.discard.content', { number: cards?.length, player })}</p>
+    <ol css={grid}>
+      {cards?.entries.map(([index, card]) =>
+        <li key={index}>
+          <MaterialComponent type={MaterialType.FactionCard} itemId={card.id} css={pointerCursorCss}
+                             onClick={() => play(displayMaterialRules(MaterialType.FactionCard, card, index))}/>
+        </li>
+      )}
+    </ol>
   </>
 }
+
+const grid = css`
+  display: grid;
+  grid-template-columns: auto auto auto;
+  list-style-type: none;
+  gap: 1em;
+  padding: 0;
+  margin: 0;
+  font-size: 1.5em;
+`
