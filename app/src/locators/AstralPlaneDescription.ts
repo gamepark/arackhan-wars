ce71a4071279a12e567584396d787123edc7ce95
Romain Fromi <romain.fromi@gmail.com>
2023-08-28 10:10:04 +0200

    Rules dialog for Astral Plane

--- app/src/locators/AstralPlaneDescription.ts
+++ app/src/locators/AstralPlaneDescription.ts
@@ -4,15 +4,14 @@ import { MaterialType } from '@gamepark/arackhan-wars/material/MaterialType'
 import { LocationType } from '@gamepark/arackhan-wars/material/LocationType'
 import { factionCardDescription } from '../material/FactionCardDescription'
 import { PlayerId } from '@gamepark/arackhan-wars/ArackhanWarsOptions'
-import { AstralPlaneRules } from './AstralPlaneRules';
-import AstralPlaneImage from '../images/locations/astral-plane.jpg'
+import { AstralPlaneRules } from './AstralPlaneRules'
+import AstralPlaneImage from '../images/locations/astral-plane.png'
 
 export class AstralPlaneDescription extends LocationDescription<PlayerId, MaterialType, LocationType> {
   height = factionCardDescription.width / factionCardDescription.ratio
   width = factionCardDescription.width
   borderRadius = 0.2
 
-  // TODO: ask an image with a better resolution
   image = AstralPlaneImage
 
   rules = AstralPlaneRules
--- app/src/locators/AstralPlaneRules.tsx
+++ app/src/locators/AstralPlaneRules.tsx
@@ -1,19 +1,14 @@
 /** @jsxImportSource @emotion/react */
-import { LocationRulesProps } from '@gamepark/react-game';
-import { Trans, useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next'
 
-export const AstralPlaneRules = (_props: LocationRulesProps) => {
+export const AstralPlaneRules = () => {
   const { t } = useTranslation()
   return <>
-    <h2>{ t('rules.astral-plane.title') }</h2>
+    <h2>{t('rules.astral-plane.title')}</h2>
     <p>
       <Trans defaults="rules.astral-plane.purpose">
         {/* TODO: ask astral icon and place it as the <0/> child */}
       </Trans>
-    </p>
-    <hr />
-    <p>
-      <Trans defaults="rules.astral-plane.note"><strong /></Trans>
     </p>
   </>
 }
