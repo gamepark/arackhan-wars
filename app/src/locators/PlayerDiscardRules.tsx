df0b7468667c59ff5930e306f4b3990c282781eb
Romain Fromi <romain.fromi@gmail.com>
2023-08-28 14:51:49 +0200

    Display card name & location in card rules dialog

--- app/src/locators/PlayerDiscardRules.tsx
+++ app/src/locators/PlayerDiscardRules.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import { LocationRulesProps, MaterialComponent, pointerCursorCss, usePlay, usePlayerName, useRules } from '@gamepark/react-game'
+import { LocationRulesProps, MaterialComponent, pointerCursorCss, usePlay, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
 import { useTranslation } from 'react-i18next'
 import { ArackhanWarsRules } from '@gamepark/arackhan-wars/ArackhanWarsRules'
 import { MaterialType } from '@gamepark/arackhan-wars/material/MaterialType'
@@ -9,13 +9,17 @@ import { css } from '@emotion/react'
 
 export const PlayerDiscardRules = ({ location }: LocationRulesProps) => {
   const { t } = useTranslation()
+  const playerId = usePlayerId()
   const player = usePlayerName(location.player)
   const cards = useRules<ArackhanWarsRules>()?.material(MaterialType.FactionCard).location(LocationType.PlayerDiscard).player(location.player)
     .sort(item => -item.location.x!)
   const play = usePlay()
   return <>
-    <h2>{t('rules.discard.title', { player })}</h2>
-    <p>{t('rules.discard.content', { number: cards?.length, player })}</p>
+    <h2>{playerId === location.player ? t('rules.discard.title.mine') : t('rules.discard.title', { player })}</h2>
+    <p>
+      {playerId === location.player ? t('rules.discard.content.mine', { number: cards?.length })
+        : t('rules.discard.content', { number: cards?.length, player })}
+    </p>
     <ol css={grid}>
       {cards?.entries.map(([index, card]) =>
         <li key={index}>
--- app/src/material/FactionCardRules.tsx
+++ app/src/material/FactionCardRules.tsx
@@ -1,17 +1,44 @@
 /** @jsxImportSource @emotion/react */
-import { MaterialRulesProps, PlayMoveButton, useLegalMove } from '@gamepark/react-game'
-import { useTranslation } from 'react-i18next'
-import { isCustomMove } from '@gamepark/rules-api'
+import { MaterialRulesProps, PlayMoveButton, useLegalMove, usePlayerId, usePlayerName } from '@gamepark/react-game'
+import { Trans, useTranslation } from 'react-i18next'
+import { displayLocationRules, isCustomMove, MaterialItem } from '@gamepark/rules-api'
 import { CustomMoveType } from '@gamepark/arackhan-wars/material/CustomMoveType'
+import { LocationType } from '@gamepark/arackhan-wars/material/LocationType'
+import { linkButtonCss } from '@gamepark/react-game/dist/css/buttonCss'
 
-export const FactionCardRules = (props: MaterialRulesProps) => {
-  const { itemIndex, closeDialog } = props
+export const FactionCardRules = ({ item, itemIndex, closeDialog }: MaterialRulesProps) => {
   const { t } = useTranslation()
   const performAction = useLegalMove(move => isCustomMove(move) && move.type === CustomMoveType.PerformAction && move.data === itemIndex)
   const chooseCard = useLegalMove(move => isCustomMove(move) && move.type === CustomMoveType.ChooseCard && move.data === itemIndex)
   return <>
+    <h2>{item.id.front ? t(`card.name.${item.id.front}`) : t('rules.card.faction', { faction: t(`faction.${item.id.back}`) })}</h2>
+    <CardLocationRule item={item}/>
     <hr/>
     {performAction && <PlayMoveButton move={performAction} onPlay={closeDialog}>{t('card.action.perform')}</PlayMoveButton>}
     {chooseCard && <PlayMoveButton move={chooseCard} onPlay={closeDialog}>{t('card.choose')}</PlayMoveButton>}
   </>
+}
+
+function CardLocationRule({ item }: { item: Partial<MaterialItem> }) {
+  const { t } = useTranslation()
+  const playerId = usePlayerId()
+  const player = usePlayerName(item.location?.player)
+  switch (item.location?.type) {
+    case LocationType.PlayerDeck:
+      return <p>{item.location?.player === playerId ? t('rules.card.deck.mine') : t('rules.card.deck', { player })}</p>
+    case LocationType.Hand:
+      return <p>{item.location?.player === playerId ? t('rules.card.hand.mine') : t('rules.card.hand', { player })}</p>
+    case LocationType.Battlefield:
+      return <p>{item.location?.player === playerId ? t('rules.card.battlefield.mine') : t('rules.card.battlefield', { player })}</p>
+    case LocationType.AstralPlane:
+      return <p>{item.location?.player === playerId ? t('rules.card.astral-plane.mine') : t('rules.card.astral-plane', { player })}</p>
+    case LocationType.PlayerDiscard:
+      return <p>
+        <Trans defaults={item.location?.player === playerId ? 'rules.card.discard.mine' : 'rules.card.discard'} values={{ player }}>
+          <PlayMoveButton css={linkButtonCss} move={displayLocationRules({ type: LocationType.PlayerDiscard, player: item.location?.player })} local/>
+        </Trans>
+      </p>
+    default:
+      return null
+  }
 }
