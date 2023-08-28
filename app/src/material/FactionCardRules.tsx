bd8bab0902fd06c4ab831de0292ce60b47831cb9
Romain Fromi <romain.fromi@gmail.com>
2023-08-28 16:36:45 +0200

    Display card kind, attack & value in rules dialog

--- app/src/material/FactionCardRules.tsx
+++ app/src/material/FactionCardRules.tsx
@@ -1,25 +1,30 @@
 /** @jsxImportSource @emotion/react */
 import { MaterialRulesProps, PlayMoveButton, useLegalMove, usePlayerId, usePlayerName } from '@gamepark/react-game'
 import { Trans, useTranslation } from 'react-i18next'
-import { displayLocationRules, isCustomMove, MaterialItem } from '@gamepark/rules-api'
+import { displayLocationRules, isCustomMove } from '@gamepark/rules-api'
 import { CustomMoveType } from '@gamepark/arackhan-wars/material/CustomMoveType'
 import { LocationType } from '@gamepark/arackhan-wars/material/LocationType'
 import { linkButtonCss } from '@gamepark/react-game/dist/css/buttonCss'
+import { onBattlefieldAndAstralPlane } from '@gamepark/arackhan-wars/material/Board'
+import { DiscardTiming } from '@gamepark/arackhan-wars/material/cards/FactionCardCharacteristics'
+import { isCreature } from '@gamepark/arackhan-wars/material/cards/Creature'
+import { isSpell } from '@gamepark/arackhan-wars/material/cards/Spell'
+import { isLand } from '@gamepark/arackhan-wars/material/cards/Land'
+import { FactionCardsCharacteristics } from '@gamepark/arackhan-wars/material/FactionCard'
 
-export const FactionCardRules = ({ item, itemIndex, closeDialog }: MaterialRulesProps) => {
+export const FactionCardRules = (props: MaterialRulesProps) => {
+  const { item, itemIndex, closeDialog } = props
   const { t } = useTranslation()
-  const performAction = useLegalMove(move => isCustomMove(move) && move.type === CustomMoveType.PerformAction && move.data === itemIndex)
   const chooseCard = useLegalMove(move => isCustomMove(move) && move.type === CustomMoveType.ChooseCard && move.data === itemIndex)
   return <>
     <h2>{item.id.front ? t(`card.name.${item.id.front}`) : t('rules.card.faction', { faction: t(`faction.${item.id.back}`) })}</h2>
-    <CardLocationRule item={item}/>
-    <hr/>
-    {performAction && <PlayMoveButton move={performAction} onPlay={closeDialog}>{t('card.action.perform')}</PlayMoveButton>}
+    <CardLocationRule {...props}/>
     {chooseCard && <PlayMoveButton move={chooseCard} onPlay={closeDialog}>{t('card.choose')}</PlayMoveButton>}
+    {item.id.front && <CardFrontRule {...props}/>}
   </>
 }
 
-function CardLocationRule({ item }: { item: Partial<MaterialItem> }) {
+const CardLocationRule = ({ item }: MaterialRulesProps) => {
   const { t } = useTranslation()
   const playerId = usePlayerId()
   const player = usePlayerName(item.location?.player)
@@ -41,4 +46,25 @@ function CardLocationRule({ item }: { item: Partial<MaterialItem> }) {
     default:
       return null
   }
+}
+
+const PerformActionButton = ({ itemIndex, closeDialog }: MaterialRulesProps) => {
+  const { t } = useTranslation()
+  const performAction = useLegalMove(move => isCustomMove(move) && move.type === CustomMoveType.PerformAction && move.data === itemIndex)
+  return performAction ? <PlayMoveButton move={performAction} onPlay={closeDialog}>{t('card.action.perform')}</PlayMoveButton> : null
+}
+
+const CardFrontRule = (props: MaterialRulesProps) => {
+  const { item } = props
+  const playerId = usePlayerId()
+  const characteristics = FactionCardsCharacteristics[item.id.front]
+  return <>
+    {isCreature(characteristics) && <p><Trans defaults="rules.card.creature" values={characteristics}><strong/></Trans></p>}
+    {isSpell(characteristics) && (
+      characteristics.discardTiming === DiscardTiming.ActivationOrEndOfTurn ? <p><Trans defaults="rules.card.spell.active"><strong/></Trans></p>
+        : <p><Trans defaults="rules.card.spell.passive"><strong/></Trans></p>
+    )}
+    {isLand(characteristics) && <p><Trans defaults="rules.card.land" values={characteristics}><strong/></Trans></p>}
+    {item.location && item.location.player === playerId && onBattlefieldAndAstralPlane(item.location) && <PerformActionButton {...props}/>}
+  </>
 }
