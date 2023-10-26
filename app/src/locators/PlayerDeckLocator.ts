f754fcb93bd3c2d79fb59df51f0660d66d1546cd
Romain Fromi <romain.fromi@gmail.com>
2023-10-26 14:13:12 +0200

    Upgrade to 6.11.2

--- app/src/locators/PlayerDeckLocator.ts
+++ app/src/locators/PlayerDeckLocator.ts
@@ -16,7 +16,7 @@ export class PlayerDeckLocator extends DeckLocator {
     return location.player === bottomPlayerId ? { x: 92.2, y: 90 } : { x: 7.7, y: 9.9 }
   }
 
-  getRotation(item: MaterialItem, context: ItemContext) {
+  getRotateZ(item: MaterialItem, context: ItemContext) {
     const bottomPlayerId = context.player ?? 1
     return item.location.player === bottomPlayerId ? 0 : 180
   }
--- app/src/locators/PlayerDiscardLocator.tsx
+++ app/src/locators/PlayerDiscardLocator.tsx
@@ -1,15 +1,21 @@
 /** @jsxImportSource @emotion/react */
 import { MaterialType } from '@gamepark/arackhan-wars/material/MaterialType'
-import { DeckLocator, MaterialContext } from '@gamepark/react-game'
-import { Location } from '@gamepark/rules-api'
+import { DeckLocator, ItemContext, MaterialContext } from '@gamepark/react-game'
+import { Location, MaterialItem } from '@gamepark/rules-api'
 import { PlayerDiscardDescription } from './PlayerDiscardDescription'
 
 export class PlayerDiscardLocator extends DeckLocator {
   locationDescription = new PlayerDiscardDescription()
   parentItemType = MaterialType.BattleMat
+  hidden = false
 
   getPositionOnParent(location: Location, context: MaterialContext) {
     const bottomPlayerId = context.player ?? 1
     return location.player === bottomPlayerId ? { x: 8.4, y: 90 } : { x: 91.4, y: 9.85 }
+  }
+
+  getRotateZ(item: MaterialItem, context: ItemContext) {
+    const bottomPlayerId = context.player ?? 1
+    return item.location.player === bottomPlayerId ? 0 : 180
   }
 }
