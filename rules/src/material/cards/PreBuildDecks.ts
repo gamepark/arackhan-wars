40ab9650d027a8242478253c8e806b6ea016c42a
Romain Fromi <romain.fromi@gmail.com>
2023-11-02 09:13:56 +0100

    Use listingToList util to simplify PreBuildDecks lists

--- rules/src/material/cards/PreBuildDecks.ts
+++ rules/src/material/cards/PreBuildDecks.ts
@@ -1,25 +1,16 @@
+import { listingToList } from '@gamepark/rules-api'
 import { Faction } from '../Faction'
+import { FactionCard } from '../FactionCard'
 import { BlightPreBuildDeck } from './blight/BlightPreBuildDeck'
 import { GreyOrderPreBuildDeck } from './greyorder/GreyOrderPreBuildDeck'
 import { NakkaPreBuildDeck } from './nakka/NakkaPreBuildDeck'
 import { WhitelandsPreBuildDeck } from './whitelands/WhitelandsPreBuildDeck'
-import { FactionCard } from '../FactionCard'
 
 export type DeckListing = Partial<Record<FactionCard, number>>
 
-const arrayFromListing = (listing: DeckListing): FactionCard[] => {
-  return Object.entries(listing).reduce<FactionCard[]>((array, [key, quantity]) => {
-    const id = parseInt(key)
-    for (let i = 0; i < quantity; i++) {
-      array.push(id)
-    }
-    return array
-  }, [])
-}
-
 export const PreBuildDecks: Record<Faction, FactionCard[]> = {
-  [Faction.Blight]: arrayFromListing(BlightPreBuildDeck),
-  [Faction.GreyOrder]: arrayFromListing(GreyOrderPreBuildDeck),
-  [Faction.Nakka]: arrayFromListing(NakkaPreBuildDeck),
-  [Faction.Whitelands]: arrayFromListing(WhitelandsPreBuildDeck)
+  [Faction.Blight]: listingToList(BlightPreBuildDeck),
+  [Faction.GreyOrder]: listingToList(GreyOrderPreBuildDeck),
+  [Faction.Nakka]: listingToList(NakkaPreBuildDeck),
+  [Faction.Whitelands]: listingToList(WhitelandsPreBuildDeck)
 }
