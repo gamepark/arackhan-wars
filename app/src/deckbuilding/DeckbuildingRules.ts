71994e03c28c291a427bd5746bd46a68bc16b946
Romain Fromi <romain.fromi@gmail.com>
2024-02-05 15:56:20 +0100

    One card per spot

--- app/src/deckbuilding/DeckbuildingRules.ts
+++ app/src/deckbuilding/DeckbuildingRules.ts
@@ -3,7 +3,8 @@ import { LocationType } from '@gamepark/arackhan-wars/material/LocationType'
 import { MaterialType } from '@gamepark/arackhan-wars/material/MaterialType'
 import { RuleId } from '@gamepark/arackhan-wars/rules/RuleId'
 import { FillGapStrategy, isEnumValue, isMoveItemType, ItemMove, MaterialGameSetup, MaterialRules, PlayerTurnRule } from '@gamepark/rules-api'
-import { range } from 'lodash'
+import difference from 'lodash/difference'
+import range from 'lodash/range'
 
 export class DeckbuildingRules extends MaterialRules<number, MaterialType, LocationType> {
   locationsStrategies = {
@@ -20,7 +21,8 @@ export class DeckbuildingRules extends MaterialRules<number, MaterialType, Locat
 class DeckbuildingRule extends PlayerTurnRule<number, MaterialType, LocationType> {
   getPlayerMoves() {
     const bookCards = this.material(MaterialType.FactionCard).location(LocationType.DeckbuildingBook)
-    return range(0, 23).flatMap(x => bookCards.moveItems({ type: LocationType.PlayerDeck, x }))
+    const deckX = this.material(MaterialType.FactionCard).location(LocationType.PlayerDeck).getItems().map(i => i.location.x!)
+    return difference(range(0, 23), deckX).flatMap(x => bookCards.moveItems({ type: LocationType.PlayerDeck, x }))
   }
 
   beforeItemMove(move: ItemMove) {
