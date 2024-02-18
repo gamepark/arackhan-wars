37cb3c4a6548c84450eb35e56e259917394af5ea
Romain Fromi <romain.fromi@gmail.com>
2024-02-18 15:10:02 +0100

    Fix Stealth

--- rules/src/rules/CardRule.ts
+++ rules/src/rules/CardRule.ts
@@ -210,7 +210,10 @@ export class CardRule extends MaterialRulesPart {
     const opponentRule = getCardRule(this.game, opponent)
     const opponentLocation = opponentRule.item.location
     if (!isXYCoordinates(cardLocation) || !isXYCoordinates(opponentLocation)) return false
-    const distance = getDistanceBetweenSquares(cardLocation, opponentLocation) + (opponentRule.hasStealth ? 1 : 0)
+    if (opponentRule.hasStealth && this.isCreature && !this.attributes.some(attribute =>
+      attribute.type === AttributeType.RangedAttack && attribute.distance > 1
+    )) return false
+    const distance = getDistanceBetweenSquares(cardLocation, opponentLocation)
     return distance === 1 || this.attributes.some(attribute =>
       attribute.type === AttributeType.RangedAttack && distance <= attribute.distance
     )
