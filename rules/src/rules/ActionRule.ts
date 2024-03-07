17bf54f8605f31c8d600a7d4487c02edf9fdb422
Romain Fromi <romain.fromi@gmail.com>
2024-03-07 14:14:48 +0100

    Remove isDeadEndMove check for action cards

--- rules/src/rules/ActionRule.ts
+++ rules/src/rules/ActionRule.ts
@@ -1,19 +1,16 @@
-import { isDeadEndMove, PlayerTurnRule } from '@gamepark/rules-api'
-import { ArackhanWarsRules } from '../ArackhanWarsRules'
+import { PlayerTurnRule } from '@gamepark/rules-api'
 import { onBattlefieldAndAstralPlane } from '../material/Board'
 import { CustomMoveType } from '../material/CustomMoveType'
 import { MaterialType } from '../material/MaterialType'
 import { Attack } from './AttackRule'
 import { getCardRule } from './CardRule'
 import { Memory } from './Memory'
-import { RuleId } from './RuleId'
 
 export class ActionRule extends PlayerTurnRule {
   getPlayerMoves() {
     return this.availableCards
       .filter(index => getCardRule(this.game, index).canPerformAction)
       .map(index => this.rules().customMove(CustomMoveType.PerformAction, index))
-      .filter(move => !isDeadEndMove(move, this.game, ArackhanWarsRules, game => game.rule?.id === RuleId.ActivationRule))
   }
 
   get availableCards() {
