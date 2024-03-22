b9c4e7c59a0943c2ee59e3ccd3e17b6b8b3a91b4
Romain Fromi <romain.fromi@gmail.com>
2024-03-22 16:00:50 +0100

    Fix Mulligan

--- app/src/locators/PlayerDeckDescription.ts
+++ app/src/locators/PlayerDeckDescription.ts
@@ -1,5 +1,9 @@
 /** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import { RuleId } from '@gamepark/arackhan-wars/rules/RuleId'
 import { LocationDescription } from '@gamepark/react-game'
+import { LocationContext } from '@gamepark/react-game/dist/locators'
+import { Location } from '../../../../workshop/packages/rules-api'
 import { factionCardDescription } from '../material/FactionCardDescription'
 import { PlayerDeckHelp } from './PlayerDeckHelp'
 
@@ -8,6 +12,21 @@ export class PlayerDeckDescription extends LocationDescription {
   ratio = factionCardDescription.ratio
   borderRadius = 0.4
   alwaysVisible = true
-  coordinates = { x: 0, y: 0, z: 0 }
+
+  getCoordinates(_location: Location, { rules }: LocationContext) {
+    return { x: 0, y: 0, z: rules.game.rule?.id === RuleId.Mulligan ? 10 : 0 }
+  }
+
+  getExtraCss(_location: Location, { rules }: LocationContext) {
+    if (rules.game.rule?.id === RuleId.Mulligan) {
+      return enlarge
+    }
+    return
+  }
+
   help = PlayerDeckHelp
 }
+
+const enlarge = css`
+  font-size: 1.2em;
+`
