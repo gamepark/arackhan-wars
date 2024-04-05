96d319641fc7bd3e948928935d86ddd6f1497da0
Romain Fromi <romain.fromi@gmail.com>
2024-04-05 09:37:59 +0200

    Prevent card from moving twice with the White Gates

--- rules/src/rules/action/TheWhiteGatesActionRule.ts
+++ rules/src/rules/action/TheWhiteGatesActionRule.ts
@@ -1,14 +1,16 @@
 import { Material } from '@gamepark/rules-api'
 import { getCardRule } from '../CardRule'
+import { Memory } from '../Memory'
 import { MoveCardsActionRule } from './MoveCardsActionRule'
 
 export class TheWhiteGatesActionRule extends MoveCardsActionRule {
   maxDistance = 2
 
   getCardsAllowedToMove(): Material {
+    const movedCards = this.remind<number[]>(Memory.MovedCards)
     const theWhiteGates = this.cardRule
     return theWhiteGates.getOtherCardsAdjacentTo()
       .player(this.player)
-      .index(index => getCardRule(this.game, index).isCreature)
+      .index(index => !movedCards.includes(index) && getCardRule(this.game, index).isCreature)
   }
 }
