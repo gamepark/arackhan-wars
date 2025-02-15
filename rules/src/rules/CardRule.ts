276cf3b4561895422631b1879f8308911fbb14a6
Romain Fromi <romain.fromi@gmail.com>
2025-02-15 10:55:45 +0100

    Bugfix Immune to enemy spell targeting

--- rules/src/rules/CardRule.ts
+++ rules/src/rules/CardRule.ts
@@ -179,7 +179,7 @@ export class CardRule extends MaterialRulesPart {
     const hasLoseSkillsWeakness = this.weaknesses.some(weakness => weakness.effects.some(isLoseSkills))
     const isImmuneToEnemySpells = this.cardsThatMightAffect.some(card =>
       card.initialSkills.some(skill => skill.effects.some(effect => effect.type === EffectType.ImmuneToEnemySpells)
-        && skill.isApplicable(this.game, this.cardMaterial, this.cardMaterial)))
+        && skill.isApplicable(this.game, card.cardMaterial, this.cardMaterial)))
     return this.cardsThatMightAffect.some(card => {
       if (isImmuneToEnemySpells && card.isSpell) return false
       const isLoseMySkills = (skill: Ability) =>
