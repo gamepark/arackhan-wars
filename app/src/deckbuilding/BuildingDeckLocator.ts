1f5f1ac6ce6978eaa7ba81404b89316ccd1e328e
Romain Fromi <romain.fromi@gmail.com>
2025-07-29 08:45:43 +0200

    Always display slot locations in deckbuilding

--- app/src/deckbuilding/BuildingDeckLocator.ts
+++ app/src/deckbuilding/BuildingDeckLocator.ts
@@ -1,18 +1,12 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import { LocationType } from '@gamepark/arackhan-wars/material/LocationType'
-import { MaterialType } from '@gamepark/arackhan-wars/material/MaterialType'
-import { DropAreaDescription, FlexLocator, MaterialContext } from '@gamepark/react-game'
+import { DropAreaDescription, FlexLocator } from '@gamepark/react-game'
 import { range } from 'lodash'
-import difference from 'lodash/difference'
 import { cardHeight, cardWidth, factionCardDescription } from '../material/FactionCardDescription'
 
 export class BuildingDeckLocator extends FlexLocator {
-  getLocations({ rules }: MaterialContext) {
-    const xWithCard = rules.material(MaterialType.FactionCard).location(LocationType.PlayerDeck).getItems().map(item => item.location.x)
-    return difference(range(0, 23), xWithCard).map(x => ({ type: LocationType.PlayerDeck, x }))
-  }
-
+  locations = range(0, 23).map(x => ({ type: LocationType.PlayerDeck, x }))
   locationDescription = new BuildingDeckDescription()
   coordinates = { x: 45 }
   gap = { x: cardWidth + 1 }
