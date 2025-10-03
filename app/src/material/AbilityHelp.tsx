a3754c2815d0818c686966225a40cf377a37b46e
Romain Fromi <romain.fromi@gmail.com>
2025-10-03 09:08:47 +0200

    Bugfix cards ability texts

--- app/src/material/AbilityHelp.tsx
+++ app/src/material/AbilityHelp.tsx
@@ -40,14 +40,14 @@ const getAbilityText = (card: FactionCard, ability: Ability, effect: Effect, t: 
     case EffectType.Attack:
       if (ability.multipliers === AbilityMultiplier.ExtraFactionToken) {
         return {
-          defaults: 'ability.attack.per.token',
+          i18nKey: 'ability.attack.per.token',
           values: {
             modifier: effect.modifier < 0 ? effect.modifier : '+' + effect.modifier
           }
         }
       } else if (Array.isArray(ability.multipliers)) {
         return {
-          defaults: 'ability.attack.per',
+          i18nKey: 'ability.attack.per',
           values: {
             multipliers: t(`targets.${ability.multipliers.map(filter => filter.text).join('.')}`,
               ability.multipliers.reduce((values, filter) => merge(values, filter.values?.(t)), {})),
@@ -56,7 +56,7 @@ const getAbilityText = (card: FactionCard, ability: Ability, effect: Effect, t: 
         }
       } else if (ability.condition) {
         return {
-          defaults: 'ability.attack.if',
+          i18nKey: 'ability.attack.if',
           values: {
             modifier: effect.modifier < 0 ? effect.modifier : '+' + effect.modifier,
             condition: ability.condition.getText(t)
@@ -64,7 +64,7 @@ const getAbilityText = (card: FactionCard, ability: Ability, effect: Effect, t: 
         }
       } else if (effect.condition !== undefined) {
         return {
-          defaults: 'ability.attack.if',
+          i18nKey: 'ability.attack.if',
           values: {
             modifier: effect.modifier < 0 ? effect.modifier : '+' + effect.modifier,
             condition: t(attackModifierConditionText[effect.condition])
@@ -72,7 +72,7 @@ const getAbilityText = (card: FactionCard, ability: Ability, effect: Effect, t: 
         }
       } else {
         return {
-          defaults: 'ability.attack.targets',
+          i18nKey: 'ability.attack.targets',
           values: {
             targets,
             modifier: effect.modifier < 0 ? effect.modifier : '+' + effect.modifier
@@ -85,7 +85,7 @@ const getAbilityText = (card: FactionCard, ability: Ability, effect: Effect, t: 
           ability.multipliers.reduce((values, filter) => merge(values, filter.values?.(t)), {}))
         if (targets) {
           return {
-            defaults: 'ability.defense.targets.per',
+            i18nKey: 'ability.defense.targets.per',
             values: {
               targets,
               multipliers,
@@ -94,7 +94,7 @@ const getAbilityText = (card: FactionCard, ability: Ability, effect: Effect, t: 
           }
         } else {
           return {
-            defaults: 'ability.defense.per',
+            i18nKey: 'ability.defense.per',
             values: {
               multipliers,
               modifier: effect.modifier < 0 ? effect.modifier : '+' + effect.modifier
@@ -103,7 +103,7 @@ const getAbilityText = (card: FactionCard, ability: Ability, effect: Effect, t: 
         }
       } else if (ability.condition) {
         return {
-          defaults: 'ability.defense.if',
+          i18nKey: 'ability.defense.if',
           values: {
             modifier: effect.modifier < 0 ? effect.modifier : '+' + effect.modifier,
             condition: ability.condition.getText(t)
@@ -111,7 +111,7 @@ const getAbilityText = (card: FactionCard, ability: Ability, effect: Effect, t: 
         }
       } else if (effect.condition === ModifyDefenseCondition.AttackedByFlyOrMoves) {
         return {
-          defaults: 'ability.defense.targets.if',
+          i18nKey: 'ability.defense.targets.if',
           values: {
             modifier: effect.modifier < 0 ? effect.modifier : '+' + effect.modifier,
             targets,
@@ -120,7 +120,7 @@ const getAbilityText = (card: FactionCard, ability: Ability, effect: Effect, t: 
         }
       } else {
         return {
-          defaults: 'ability.defense.targets',
+          i18nKey: 'ability.defense.targets',
           values: {
             targets,
             modifier: effect.modifier < 0 ? effect.modifier : '+' + effect.modifier
@@ -130,59 +130,59 @@ const getAbilityText = (card: FactionCard, ability: Ability, effect: Effect, t: 
     case EffectType.LoseAttributes:
       if (effect.attributes?.length === 1) {
         return {
-          defaults: 'ability.attribute.lose',
+          i18nKey: 'ability.attribute.lose',
           values: { targets, attribute: t(`attribute.${effect.attributes[0]}`) }
         }
       } else {
-        return { defaults: 'ability.attributes.lose', values: { targets } }
+        return { i18nKey: 'ability.attributes.lose', values: { targets } }
       }
     case EffectType.GainAttributes: {
       const attribute = effect.attributes[0]
       if (!targets) {
         return {
-          defaults: 'ability.attribute.gain.if',
+          i18nKey: 'ability.attribute.gain.if',
           values: { attribute: t(`attribute.${attribute.type}`, attribute), condition: ability.condition!.getText(t) }
         }
       } else if (!ability.condition) {
         return {
-          defaults: 'ability.attribute.give',
+          i18nKey: 'ability.attribute.give',
           values: { targets, attribute: t(`attribute.${attribute.type}`, attribute) }
         }
       } else {
         return {
-          defaults: 'ability.attribute.give.if',
+          i18nKey: 'ability.attribute.give.if',
           values: { targets, attribute: t(`attribute.${attribute.type}`, attribute), condition: ability.condition.getText(t) }
         }
       }
     }
     case EffectType.LoseSkills:
-      return { defaults: 'ability.skills.lose', values: { targets } }
+      return { i18nKey: 'ability.skills.lose', values: { targets } }
     case EffectType.Deactivated:
-      return { defaults: 'ability.deactivate', values: { targets } }
+      return { i18nKey: 'ability.deactivate', values: { targets } }
     case EffectType.Trigger:
       switch (effect.condition) {
         case TriggerCondition.Attack:
           if (effect.action === TriggerAction.Destroy) {
             if (targets) {
-              return { defaults: 'ability.targets.trigger.attack.destroy', values: { targets } }
+              return { i18nKey: 'ability.targets.trigger.attack.destroy', values: { targets } }
             } else {
-              return { defaults: 'ability.trigger.attack.destroy', values: { card: t(`card.name.${getUniqueCard(card)}`) } }
+              return { i18nKey: 'ability.trigger.attack.destroy', values: { card: t(`card.name.${getUniqueCard(card)}`) } }
             }
           }
           break
         case TriggerCondition.FailAttack:
           if (effect.action === TriggerAction.Destroy) {
-            return { defaults: 'ability.trigger.fail-attack.destroy', values: { card: t(`card.name.${getUniqueCard(card)}`) } }
+            return { i18nKey: 'ability.trigger.fail-attack.destroy', values: { card: t(`card.name.${getUniqueCard(card)}`) } }
           }
           break
         case TriggerCondition.DestroyFlyOrMove:
           if (effect.action === TriggerAction.PutCardUnder) {
-            return { defaults: 'ability.trigger.dragon-trophy', values: { card: t(`card.name.${getUniqueCard(card)}`) } }
+            return { i18nKey: 'ability.trigger.dragon-trophy', values: { card: t(`card.name.${getUniqueCard(card)}`) } }
           }
           break
         case TriggerCondition.EndOfRound:
           if (effect.action === TriggerAction.Destroy) {
-            return { defaults: 'ability.trigger.end-round.destroy', values: { targets } }
+            return { i18nKey: 'ability.trigger.end-round.destroy', values: { targets } }
           }
           break
       }
@@ -196,14 +196,14 @@ const getAbilityText = (card: FactionCard, ability: Ability, effect: Effect, t: 
         if (targets) {
           values.targets = targets
           return {
-            defaults: effect.limitation ?
+            i18nKey: effect.limitation ?
               `ability.targets.attack.limit.${attackLimitationText[effect.limitation]}`
               : 'ability.targets.attack.limit',
             values
           }
         } else {
           return {
-            defaults: effect.limitation ?
+            i18nKey: effect.limitation ?
               `ability.attack.limit.${attackLimitationText[effect.limitation]}`
               : 'ability.attack.limit',
             values
@@ -213,13 +213,13 @@ const getAbilityText = (card: FactionCard, ability: Ability, effect: Effect, t: 
     }
     case EffectType.CanOnlyAttack:
       if (targets) {
-        return { defaults: `ability.targets.attack.condition.${attackConditionText[effect.condition]}`, values: { targets } }
+        return { i18nKey: `ability.targets.attack.condition.${attackConditionText[effect.condition]}`, values: { targets } }
       } else {
-        return { defaults: `ability.attack.condition.${attackConditionText[effect.condition]}` }
+        return { i18nKey: `ability.attack.condition.${attackConditionText[effect.condition]}` }
       }
     case EffectType.CannotBeAttacked:
       return {
-        defaults: effect.limitation ?
+        i18nKey: effect.limitation ?
           `ability.attacked.limit.${attackLimitationText[effect.limitation]}`
           : 'ability.attacked.limit',
         values: { targets, card: t(`card.name.${getUniqueCard(card)}`) }
@@ -227,49 +227,49 @@ const getAbilityText = (card: FactionCard, ability: Ability, effect: Effect, t: 
     case EffectType.CanOnlyBeAttacked:
       if (targets) {
         return {
-          defaults: `ability.targets.attacked.condition.${attackConditionText[effect.condition]}`,
+          i18nKey: `ability.targets.attacked.condition.${attackConditionText[effect.condition]}`,
           values: { targets, card: t(`card.name.${getUniqueCard(card)}`) }
         }
       } else {
         return {
-          defaults: `ability.attacked.condition.${attackConditionText[effect.condition]}`,
+          i18nKey: `ability.attacked.condition.${attackConditionText[effect.condition]}`,
           values: { card: t(`card.name.${getUniqueCard(card)}`) }
         }
       }
     case EffectType.ImmuneToEnemySpells:
       if (ability.condition) {
-        return { defaults: 'ability.spell-immune.if', values: { condition: ability.condition.getText(t) } }
+        return { i18nKey: 'ability.spell-immune.if', values: { condition: ability.condition.getText(t) } }
       } else if (targets) {
-        return { defaults: 'ability.spell-immune.targets', values: { targets } }
+        return { i18nKey: 'ability.spell-immune.targets', values: { targets } }
       } else {
-        return { defaults: 'ability.spell-immune' }
+        return { i18nKey: 'ability.spell-immune' }
       }
     case EffectType.EndOfTurn:
-      return { defaults: 'ability.end-of-turn-move', values: { card: t(`card.name.${getUniqueCard(card)}`) } }
+      return { i18nKey: 'ability.end-of-turn-move', values: { card: t(`card.name.${getUniqueCard(card)}`) } }
     case EffectType.IgnoreAttackDefenseModifiers:
-      return { defaults: 'ability.ignore-modifiers', values: { card: t(`card.name.${getUniqueCard(card)}`) } }
+      return { i18nKey: 'ability.ignore-modifiers', values: { card: t(`card.name.${getUniqueCard(card)}`) } }
     case EffectType.ModifyMovement:
       if (effect.conditions.includes(ModifyMovementCondition.DoNotAttack)
         && effect.conditions.includes(ModifyMovementCondition.EndMovementAdjacentToEnemyCard)) {
-        return { defaults: 'ability.modify.movement.if', values: { modifier: effect.modifier, card: t(`card.name.${getUniqueCard(card)}`) } }
+        return { i18nKey: 'ability.modify.movement.if', values: { modifier: effect.modifier, card: t(`card.name.${getUniqueCard(card)}`) } }
       }
       return {}
     case EffectType.InvertsAttackDefense:
-      return { defaults: 'ability.inverts-attack-defense', values: { targets } }
+      return { i18nKey: 'ability.inverts-attack-defense', values: { targets } }
     case EffectType.ModifyRange:
-      return { defaults: 'ability.range.add.targets', values: { targets, modifier: effect.modifier } }
+      return { i18nKey: 'ability.range.add.targets', values: { targets, modifier: effect.modifier } }
     case EffectType.ExtraScore:
       switch (effect.score) {
         case ExtraScoreType.ValueOfCardsUnder:
-          return { defaults: 'ability.score.cards-under', values: { card: t(`card.name.${getUniqueCard(card)}`) } }
+          return { i18nKey: 'ability.score.cards-under', values: { card: t(`card.name.${getUniqueCard(card)}`) } }
         case ExtraScoreType.MastersOfAracKhan:
-          return { defaults: 'ability.score.masters' }
+          return { i18nKey: 'ability.score.masters' }
         default:
           return {}
       }
     case EffectType.IgnoreFellowGroupAttackWeakness:
       return {
-        defaults: 'ability.ignore-fellow-weakness',
+        i18nKey: 'ability.ignore-fellow-weakness',
         values: {
           targets: t(`targets.${effect.filters.map(filter => filter.text).join('.')}`,
             effect.filters.reduce((values, filter) => merge(values, filter.values?.(t)), {})),
@@ -278,13 +278,13 @@ const getAbilityText = (card: FactionCard, ability: Ability, effect: Effect, t: 
       }
     case EffectType.CannotBePlayed:
       if (effect.limitation === RoundLimitation.LastRound) {
-        return { defaults: 'ability.cannot-place.last-turn' }
+        return { i18nKey: 'ability.cannot-place.last-turn' }
       }
       return {}
     case EffectType.HitAllies:
-      return { defaults: 'ability.hit-allies', values: { card: t(`card.name.${getUniqueCard(card)}`) } }
+      return { i18nKey: 'ability.hit-allies', values: { card: t(`card.name.${getUniqueCard(card)}`) } }
     case EffectType.SwarmSameCard:
-      return { defaults: 'ability.swarm-same', values: { card: t(`card.name.${getUniqueCard(card)}`) } }
+      return { i18nKey: 'ability.swarm-same', values: { card: t(`card.name.${getUniqueCard(card)}`) } }
     default:
       return {}
   }
